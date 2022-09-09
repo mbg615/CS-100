ef93a0c4d51e5908a969f816735ed09653d8a448
Maddox Guthrie <maddox@guthriehouse.rocks>
2022-09-09 08:53:09 -0500

    Add solution for Project 1

--- Project1/p1.c
+++ Project1/p1.c
@@ -2,8 +2,34 @@
 // by: Maddox Guthrie
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    //Code to be released on 9/5/21
+    int numOf20s, numOf10s, numOf5s, numOf1s, numOfQuarters, numOfDimes, numOfNickels, numOfPennies, total;
+    double lengthOfMoney;
+
+    printf("Enter the number of $20s: ");
+    scanf("%d", &numOf20s);
+    printf("Enter the number of $10s: ");
+    scanf("%d", &numOf10s);
+    printf("Enter the number of $5s: ");
+    scanf("%d", &numOf5s);
+    printf("Enter the number of $1s: ");
+    scanf("%d", &numOf1s);
+    printf("Enter the number of quarters: ");
+    scanf("%d", &numOfQuarters);
+    printf("Enter the number of dimes: ");
+    scanf("%d", &numOfDimes);
+    printf("Enter the number of nickels: ");
+    scanf("%d", &numOfNickels);
+    printf("Enter the number of pennies: ");
+    scanf("%d", &numOfPennies);
+
+    total = ((numOf20s * 20 + numOf10s * 10 + numOf5s * 5 + numOf1s) * 100) + numOfQuarters * 25 + numOfDimes * 10 + numOfNickels * 5 + numOfPennies;
+    printf("You have %d dollars and %d cents\n", total / 100, total % 100);
+    
+    lengthOfMoney = ((numOf20s + numOf10s + numOf5s + numOf1s) * 6.14) + numOfQuarters * 0.955 + numOfDimes * 0.705 + numOfNickels * 0.835 + numOfPennies * 0.750;
+    printf("The length of your money is roughly %d inches\n", (int)round(lengthOfMoney));
+
     return 0;
 }
