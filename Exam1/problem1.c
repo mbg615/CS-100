0039cb2f7c464e765137212a7270eebdbfa8cf38
Maddox Guthrie <maddox@guthriehouse.rocks>
2022-09-21 09:13:35 -0500

    add solution for exam 1 problems

---
+++ Exam1/problem1.c
@@ -0,0 +1,21 @@
+// CS-100 Test 1: file1.c
+// by: Maddox Guthrie
+
+#include <stdio.h>
+
+int main() {
+    int numEntered, score = 0;
+    scanf("%d", &numEntered);
+
+    while(numEntered != 0) {
+        if(numEntered % 2 == 0) {
+            score += numEntered;
+        } else {
+            score -= numEntered;
+        }
+        scanf("%d", &numEntered);
+    }
+    printf("%d\n", score);
+
+    return 0;
+}
---
+++ Exam1/problem2.c
@@ -0,0 +1,22 @@
+// CS-100 Test 1: file2.c
+// by: Maddox Guthrie
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char wordEntered[240];
+    scanf("%s", wordEntered);
+
+    for(int i = 0; i <= strlen(wordEntered); i++) {
+        if(islower(wordEntered[i])) {
+            wordEntered[i] = toupper(wordEntered[i]);
+        } else if(isupper(wordEntered[i])) {
+            wordEntered[i] = tolower(wordEntered[i]);
+        }
+    }
+    printf("%s\n", wordEntered);
+
+    return 0;
+}
